15c26a0ffa9e7707ac3d0ea5f1a393b5f2b89c37
Putu Krisna Andyartha <putukrisna6@gmail.com>
2022-05-26 22:38:53 +0700

    feat: ls functionality

--- bagA/jsonrpc_server.py
+++ bagA/jsonrpc_server.py
@@ -4,16 +4,30 @@
 # JSON-RPC server needing "pip install jsonrpclib-pelix"
 
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+import glob
 
 def ping(*args):
     results = []
-
     results.append(('Size', len(args)))
 
     i = 1
     for arg in args:
         results.append(('Message_{}'.format(i), arg))
         i += 1
+
+    return results
+
+def ls(*args):
+    results = []
+    argSize = len(args)
+
+    if argSize == 1:
+        globbed = glob.glob('*')
+    else:
+        globbed = glob.glob(args[1])
+
+    for i in globbed:
+        results.append(i)
 
     return results
 
