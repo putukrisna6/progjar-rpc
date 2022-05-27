28200e9a5e5db69fecbef3341667f4cc5ea3b7c4
Putu Krisna Andyartha <putukrisna6@gmail.com>
2022-05-27 20:15:23 +0700

    feat: make Facade for client

--- bagB/rpyc_client.py
+++ bagB/rpyc_client.py
@@ -6,27 +6,38 @@
 import rpyc
 import os
 
+class FileManageFacade:
+    def __init__(self):
+        config = {'allow_public_attrs': True}
+        self.proxy = rpyc.connect('localhost', 18861, config=config)
+
+    def line_counter(self, fileobj, noisy):
+        return self.proxy.root.line_counter(fileobj, noisy)
+
+    def ping(self, cmd):
+        return self.proxy.ping(*cmd)
+
+    def ls(self, cmd):
+        return self.proxy.ls(*cmd)
+
+    def count(self, cmd):
+        return self.proxy.count(*cmd)
+
+    def get(self, cmd):
+        return self.proxy.get(*cmd)
+
+    def send(self, cmd):
+        return self.proxy.send(*cmd)
+
 def main():
-    config = {'allow_public_attrs': True}
-    proxy = rpyc.connect('localhost', 18861, config=config)
-    # fileobj = open('testfile.txt')
-    # linecount = proxy.root.line_counter(fileobj, noisy)
-    # print('sek')
+    facade = FileManageFacade()
 
-    def ls(cmd):
-        print(proxy.root.ls(cmd))
+    fileobj = open('testfile.txt')
+    linecount = facade.line_counter(fileobj, noisy)
+    print('The number of lines in the file was', linecount)
 
-    def count(cmd):
-        print(proxy.root.count(cmd))
-
-    def put(cmd):
-        print(proxy.root.put(cmd))
-
-    def get(cmd):
-        print(proxy.root.get(cmd))
-
-    def quit(cmd):
-        print(proxy.root.quit(cmd))
+def noisy(string):
+    print('Noisy:', repr(string))
 
 if __name__ == '__main__':
     main()
