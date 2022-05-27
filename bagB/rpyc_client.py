266fcf9b5ffc2dd7b710187edcaebf823f53a1b2
Putu Krisna Andyartha <putukrisna6@gmail.com>
2022-05-27 20:52:16 +0700

    feat: implement count and ls

--- bagB/rpyc_client.py
+++ bagB/rpyc_client.py
@@ -5,6 +5,7 @@
 
 import rpyc
 import os
+import json
 
 class FileManageFacade:
     def __init__(self):
@@ -15,26 +16,49 @@ class FileManageFacade:
         return self.proxy.root.line_counter(fileobj, noisy)
 
     def ping(self, cmd):
-        return self.proxy.ping(*cmd)
+        return self.proxy.root.ping(*cmd)
 
     def ls(self, cmd):
-        return self.proxy.ls(*cmd)
+        return self.proxy.root.ls(*cmd)
 
     def count(self, cmd):
-        return self.proxy.count(*cmd)
+        return self.proxy.root.count(*cmd)
 
     def get(self, cmd):
-        return self.proxy.get(*cmd)
+        return self.proxy.root.get(*cmd)
 
     def send(self, cmd):
-        return self.proxy.send(*cmd)
+        return self.proxy.root.send(*cmd)
+
+def jsonHelper(response):
+    print(json.dumps(response, indent=4))
+
+def printHelper(replies):
+    for r in replies:
+        print(r)
 
 def main():
     facade = FileManageFacade()
 
-    fileobj = open('testfile.txt')
-    linecount = facade.line_counter(fileobj, noisy)
-    print('The number of lines in the file was', linecount)
+    # fileobj = open('testfile.txt')
+    # linecount = facade.line_counter(fileobj, noisy)
+    # print('The number of lines in the file was', linecount)
+    cmd = ''
+    while (cmd != 'quit'):
+        cmd = input()
+        cmds = cmd.split()
+
+        if cmds[0] == 'ping':
+            printHelper(facade.ping(cmds))
+        elif cmds[0] == 'ls':
+            printHelper(facade.ls(cmds))
+        elif cmds[0] == 'count':
+            printHelper(facade.count(cmds))
+        # elif cmds[0] == 'get':
+        #     getHelper(facade.get(cmds))
+        # elif cmds[0] == 'send':
+        #     fileData = sendHelper(cmds)
+        #     jsonHelper(facade.send(fileData))
 
 def noisy(string):
     print('Noisy:', repr(string))
--- bagB/rpyc_server.py
+++ bagB/rpyc_server.py
@@ -6,39 +6,83 @@
 import rpyc
 import glob
 import os
+import base64
 
 def main():
     from rpyc.utils.server import ThreadedServer
-    server = ThreadedServer(MyService, port = 18861)
-    server.start()
+    t = ThreadedServer(MyService, port = 18861)
+    t.start()
 
 class MyService(rpyc.Service):
-    # def exposed_line_counter(self, fileobj, function):
-    #     print('Client has invoked exposed_line_counter()')
-    #     for linenum, line in enumerate(fileobj.readlines()):
-    #         function(line)
-    #     return linenum + 1
-    def ls(self, input):
-        return input
+    def exposed_line_counter(self, fileobj, function):
+        print('Client has invoked exposed_line_counter()')
+        for linenum, line in enumerate(fileobj.readlines()):
+            function(line)
+        return linenum + 1
 
-    def count(self, input):
+    def exposed_ping(self, *args):
         results = []
-        argsLen = len(input)
+        words = args[1:]
+        argsLen = len(words)
+
+        if argsLen == 0:
+            response = {
+                "success": False,
+                "errorMsg": "Invalid argument count for ping@FileManageFacade"
+            }
+            results.append(response)
+            return results
+
+        msgs = []
+        for i in words:
+            msgs.append(i)
+
+        response = {
+            "success": True,
+            "result": {
+                "wordCount": argsLen,
+                "messages": msgs
+            }
+        }
+
+        results.append(response)
+        return results
+
+    def exposed_ls(self, *args):
+        results = []
+        argsLen = len(args)
 
         if argsLen == 1:
             globbed = glob.glob('*')
         else:
-            globbed = glob.glob(input[1])
+            globbed = glob.glob(args[1])
+
+        response = {
+            "success": True,
+            "result": {
+                "contents": globbed
+            }
+        }
+        results.append(response)
         return results
 
-    def put(self, input):
-        return input
+    def exposed_count(self, *args):
+        results = []
+        argsLen = len(args)
 
-    def get(self, input):
-        return input
+        if argsLen == 1:
+            globbed = glob.glob('*')
+        else:
+            globbed = glob.glob(args[1])
 
-    def quit(self, input):
-        return input
+        response = {
+            "success": True,
+            "result": {
+                "count": len(globbed)
+            }
+        }
+        results.append(response)
+        return results
 
 if __name__ == '__main__':
     main()
