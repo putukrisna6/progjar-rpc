3932db15bde5205b003839519a745d5a56737140
daffainfo <md15ev@gmail.com>
2022-05-27 19:50:19 +0700

    Add some unused func

--- bagB/rpyc_client.py
+++ bagB/rpyc_client.py
@@ -4,16 +4,29 @@
 # RPyC client
 
 import rpyc
+import os
 
 def main():
     config = {'allow_public_attrs': True}
     proxy = rpyc.connect('localhost', 18861, config=config)
-    fileobj = open('testfile.txt')
-    linecount = proxy.root.line_counter(fileobj, noisy)
-    print('The number of lines in the file was', linecount)
+    # fileobj = open('testfile.txt')
+    # linecount = proxy.root.line_counter(fileobj, noisy)
+    # print('sek')
 
-def noisy(string):
-    print('Noisy:', repr(string))
+    def ls(cmd):
+        print(proxy.root.ls(cmd))
+
+    def count(cmd):
+        print(proxy.root.count(cmd))
+
+    def put(cmd):
+        print(proxy.root.put(cmd))
+
+    def get(cmd):
+        print(proxy.root.get(cmd))
+
+    def quit(cmd):
+        print(proxy.root.quit(cmd))
 
 if __name__ == '__main__':
     main()
--- bagB/rpyc_server.py
+++ bagB/rpyc_server.py
@@ -4,18 +4,34 @@
 # RPyC server
 
 import rpyc
+import glob
+import os
 
 def main():
     from rpyc.utils.server import ThreadedServer
-    t = ThreadedServer(MyService, port = 18861)
-    t.start()
+    server = ThreadedServer(MyService, port = 18861)
+    server.start()
 
 class MyService(rpyc.Service):
-    def exposed_line_counter(self, fileobj, function):
-        print('Client has invoked exposed_line_counter()')
-        for linenum, line in enumerate(fileobj.readlines()):
-            function(line)
-        return linenum + 1
+    # def exposed_line_counter(self, fileobj, function):
+    #     print('Client has invoked exposed_line_counter()')
+    #     for linenum, line in enumerate(fileobj.readlines()):
+    #         function(line)
+    #     return linenum + 1
+    def ls(self, input):
+        return input
+
+    def count(self, input):
+        return input
+
+    def put(self, input):
+        return input
+
+    def get(self, input):
+        return input
+
+    def quit(self, input):
+        return input
 
 if __name__ == '__main__':
     main()
